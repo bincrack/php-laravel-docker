cf28033d07afe0bfe64821b7391582405951afc4
admin <admin@gmail.com>
2024-11-04 19:53:32 +0800

    Update nikkei.php

--- public/nikkei.php
+++ public/nikkei.php
@@ -1,3 +1,26 @@
 <?php
+$headers_ignore = array('host', 'connection');
+$headers = [  
+    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',  
+    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',  
+];  
+foreach ($_SERVER as $key => $value) {
+    if (substr($key, 0, 5) === 'HTTP_') {
+        $key = substr($key, 5);
+        $key = str_replace('_', '-', $key);
+        $key = strtolower($key);
+        if (!in_array($key, $headers_ignore)) {
+            $headers[$key] = $value;
+        }
+    }
+}
+var_dump($headers);
+
+$scheme = isset( $_REQUEST[ 'scheme' ] ) && !empty( $_REQUEST[ 'scheme' ] ) ? $_REQUEST[ 'scheme' ] : 'https';
+$path = isset( $_REQUEST[ 'path' ] ) && !empty( $_REQUEST[ 'path' ] ) ? $_REQUEST[ 'path' ] : 'www.baidu.com';
+$url = $scheme."://".$path;
+var_dump($scheme);
+var_dump($path);
+var_dump($url);
 phpinfo();
 ?>
