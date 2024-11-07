2e1b24abc6a143eaf29eb3887f050be537a3becb
admin <admin@gmail.com>
2024-11-07 21:40:44 +0800

    Update nikkei.php

--- public/nikkei.php
+++ public/nikkei.php
@@ -1,5 +1,5 @@
 <?php
-$headers_ignore = array('host', 'connection', 'x-forwarded-for', 'true-client-ip', 'cf-connecting-ip');
+$headers_ignore = array('host', 'connection', 'x-forwarded-for', 'true-client-ip', 'cf-connecting-ip', 'accept-encoding');
 $headers_req = array();
 $headers_def_req = array(
     'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',  
@@ -18,6 +18,116 @@ foreach ($_SERVER as $key => $value) {
 }
 foreach ($headers_def_req as $key => $value) {
     array_push($headers_req, $key.': '.$value);
+}
+$link_url = null;
+$link_urls = array();
+function to_tag($tag, $tag_name) {
+    global $link_urls;
+    $tag_val = $tag->getAttribute($tag_name);
+    if (empty($tag_val)) {
+        return;
+    }
+    if (strpos($tag_val, 'http://') === 0) {
+        $tag->setAttribute($tag_name, ''.substr($tag_val, 7));
+    } elseif (strpos($tag_val, 'https://') === 0) {
+        $tag->setAttribute($tag_name, ''.substr($tag_val, 8));
+    } elseif (strpos($tag_val, '//') === 0) {
+        $tag->setAttribute($tag_name, ''.substr($tag_val, 2));
+    } elseif (strpos($tag_val, '/') === 0) {
+    }
+    array_push($link_urls, array($tag->getAttribute($tag_name), $tag_val));
+}
+$hook_script = <<<EOT
+function function_hook(func, obj, arg) {
+    var url = null;
+    var tag = arg[0];
+    if (typeof tag === 'object') {
+        if (tag.tagName === 'SCRIPT') {
+            url = tag.src;
+            if (tag.src.indexOf('https://') === 0) {
+                tag.src = tag.src.substr(8);
+            }
+        } else if (tag.tagName === 'LINK') {
+            url = tag.href;
+            if (tag.href.indexOf('https://') === 0) {
+                tag.href = tag.href.substr(8);
+            }
+        }
+
+        if (url) {
+            console.log('hook', func.name, url);
+        }
+    }
+    func.apply(obj, arg);
+}
+function function_hook_head_insert() {
+    var h = document.getElementsByTagName("head");
+    var tmp = h[0].insertBefore;
+    h[0].insertBefore = function() {
+        function_hook(tmp, this, arguments);
+    }
+}
+function function_hook_head_append() {
+    var h = document.getElementsByTagName("head");
+    var tmp = h[0].appendChild;
+    h[0].appendChild = function() {
+        function_hook(tmp, this, arguments);
+    }
+}
+function function_hook_ajax() {
+    var tmp = window.XMLHttpRequest.prototype.open;
+    window.XMLHttpRequest.prototype.open = function() {
+        console.log('Request', arguments);
+        // tmp.apply(this, arguments);
+    }
+}
+function_hook_head_insert();
+function_hook_head_append();
+function_hook_ajax();
+// debugger;
+EOT;
+function to_url($body, $headers) {
+    global $hook_script;
+    $content_type = $headers['content-type'];
+    if (empty($content_type)) {
+        return $body;
+    }
+
+    list($type, $subType) = explode(';', $content_type);
+    if ($type == 'text/css' || $type == 'image/png' || $type == 'text/javascript') {
+        return $body;
+    }
+
+    $dom = new DOMDocument();
+    $dom->loadHTML($body);
+    $links = $dom->getElementsByTagName('link');
+    foreach ($links as $tag) {
+        to_tag($tag, 'href');
+    }
+
+    $links = $dom->getElementsByTagName('img');
+    foreach ($links as $tag) {
+        to_tag($tag, 'src');
+    }
+
+    $links = $dom->getElementsByTagName('script');
+    foreach ($links as $tag) {
+        to_tag($tag, 'src');
+    }
+    $head = $dom->getElementsByTagName('head');
+    if ($head->length) {
+        $child = null;
+        $script = $dom->getElementsByTagName('script');
+        if ($script->length) {
+            $child = $script->item(0);
+        }
+        $element = $dom->createElement('script'); 
+        $element->nodeValue = $hook_script;
+
+        $head->item(0)->insertBefore($element, $child);
+    }
+
+    return $dom->saveHTML();
 }
 
 $debug = isset( $_REQUEST[ 'debug' ] ) && !empty( $_REQUEST[ 'debug' ] ) ? $_REQUEST[ 'debug' ] : '0';
@@ -74,7 +184,7 @@ if (curl_errno($ch)) {
         }
     }
     if ($http_code == 200 || $http_code == 400 || $http_code == 403) {
-        echo $body;
+        echo to_url($body, $headers_res);
     } elseif ($http_code == 301 || $http_code == 302) {
         echo sprintf("<div class='center'>跳转地址: <a href='%s'>%s</a></div>", $redirect, $redirect);
     } else {
@@ -87,6 +197,7 @@ $page_info = array(
     "scheme" => $scheme,
     "path" => $path,
     "url" => $url,
+    "link_urls" => $link_urls,
     "http_code" => $http_code
 );
 if ($debug == '1') {
